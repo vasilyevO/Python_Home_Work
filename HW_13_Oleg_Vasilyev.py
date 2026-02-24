ee303ef483d378dfeec97cbaa6bf4c2744c6b184
vasilyevO <vasilyev.o@gmail.com>
2026-02-24 15:46:12 +0100

    Home_Work_13_fixed

--- HW_13_Oleg_Vasilyev.py
+++ HW_13_Oleg_Vasilyev.py
@@ -1,27 +1,28 @@
 # Home work 13
+#Исправленное задание 2
 
 # Прогрессия увеличения
-numbers = (3, 7, 2, 8, 5, 10, 1)
-result = (numbers[0],)
-max_value = numbers[0]
+# numbers = (3, 7, 2, 8, 5, 10, 1)
+# result = (numbers[0],)
+# max_value = numbers[0]
+#
+# for num in numbers[1:]:
+#     if num > max_value:
+#         result += (num,)
+#         max_value = num
 
-for num in numbers[1:]:
-    if num > max_value:
-        result += (num,)
-        max_value = num
-
-print("Кортеж по возрастанию:", result)
+# print("Кортеж по возрастанию:", result)
 
 # 2. Повторяющиеся элементы
 numbers = (1, 2, 3, 4, 2, 5, 3, 6, 4, 2, 9)
-checked = ()
-
+checked = []
 for value in numbers:
     if value not in checked and numbers.count(value) > 1:
-        indexes = ()
+        checked.append(value)
+        indexes = []
+
         for index, item in enumerate(numbers):
             if item == value:
-                indexes += (index,)
+                indexes.append(index)
 
-        print(f"Индекс элемента {value}: {indexes}")
-        checked += (value,)
+        print(f"Индексы элемента {value}:", *indexes)
