93d925b259e3454438d87cd5a4e2b90be99b06eb
vasilyevO <vasilyev.o@gmail.com>
2026-02-24 16:02:21 +0100

    Home_Work_13_Fixed

--- HW_13_Oleg_Vasilyev.py
+++ HW_13_Oleg_Vasilyev.py
@@ -11,9 +11,20 @@
 #         result += (num,)
 #         max_value = num
 
-# print("Кортеж по возрастанию:", result)
+print("1. Прогрессия увеличения.")
+numbers = (3, 7, 2, 8, 5, 10, 1)
+print("Исходные значения: ", numbers)
+result = [numbers[0]]
+max_value = numbers[0]
 
-# 2. Повторяющиеся элементы
+for num in numbers[1:]:
+    if num > max_value:
+        result.append(num)
+        max_value = num
+
+print("Кортеж по возрастанию:", tuple(result))
+
+print("2. Повторяющиеся элементы.")
 numbers = (1, 2, 3, 4, 2, 5, 3, 6, 4, 2, 9)
 checked = []
 for value in numbers:
