cef42bfcf21a78262860efde9458f27a15a6a2d2
vasilyevO <vasilyev.o@gmail.com>
2026-03-25 00:02:50 +0100

    test

--- test.py
+++ test.py
@@ -1,1 +1,26 @@
-git push -u origin main
+text = "I have 5 apples and 10 oranges, price is 0.5 each. Card number is ....3672."
+
+result = ""
+number = ""
+dot_used = False
+
+for ch in text:
+    if ch.isdigit():
+        number += ch
+
+    elif ch == "." and number != "" and not dot_used:
+        number += ch
+        dot_used = True
+
+    else:
+        if number != "":
+            result += str(float(number) * 10)
+            number = ""
+            dot_used = False
+
+        result += ch
+
+if number != "":
+    result += str(float(number) * 10)
+
+print(result)
